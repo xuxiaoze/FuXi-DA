25bee5191f612853df772c52d3f0396cbbeac857
xuxiaoze <103574293+xuxiaoze@users.noreply.github.com>
2025-04-16 20:16:28 +0800

    Update run_inference.sh

--- run_inference.sh
+++ run_inference.sh
@@ -1,6 +1,4 @@
 #!/bin/bash 
-export PATH="/cpfs01/projects-HDD/cfff-4a8d9af84f66_HDD/public/xuxiaoze/miniconda3/envs/pytorch/bin:$PATH"
-
 python inference.py \
 --model_dir ../model \
 --fcst_dir ../bg \
